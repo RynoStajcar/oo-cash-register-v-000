1270e755b954fef2e3a87c95c253ef8ec0e623a4
Ryne Stajcar <rynostajcar@gmail.com>
2018-01-22 04:35:17 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -18,7 +18,7 @@ attr_accessor :total, :discount, :items
 
   def apply_discount
     if discount > 0
-      self.total = total - total.percent(discount)
+      self.total = total - total.percent_of(discount)
       "After the discount, the total comes to $#{self.total}."
     else "There is no discount to apply."
     end
