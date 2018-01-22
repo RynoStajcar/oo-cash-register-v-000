cc62d6140fd5b2d13dfb27f958083bf6d11d6937
Ryne Stajcar <rynostajcar@gmail.com>
2018-01-22 04:48:49 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -26,5 +26,5 @@ attr_accessor :total, :discount, :items :last_transaction
   end
 
   def void_last_transaction
-    self.total = total - last_transaction
+    self.total = self.total - self.last_transaction
 end
