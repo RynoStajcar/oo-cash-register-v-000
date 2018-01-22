f7b3f61bfde6cc53992dd72474160c36c0d5417f
Ryne Stajcar <rynostajcar@gmail.com>
2018-01-22 04:31:57 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -10,7 +10,7 @@ attr_accessor :total, :discount, :items
   end
 
   def add_item(title, price, quantity = 1)
-    self.total = total + price * quantity%
+    self.total = total + price * quantity
     quantity.times do
       items << title
     end
@@ -18,7 +18,7 @@ attr_accessor :total, :discount, :items
 
   def apply_discount
     if discount > 0
-      self.total = total - discount
+      self.total = total - discount%
       "After the discount, the total comes to $#{self.total}."
     else "There is no discount to apply."
     end
